960ff0c2ea2cb0186a6a6847ab9db8c1faa9025a
sukrit007 <sukrit007@gmail.com>
2018-01-31 08:28:48 -0700

    Update template structure and add config service

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ const paths = {
   dist: 'dist',
   artifacts: [
     'modules/totem-v3/provisioning/*.yml',
-    'modules/totem-v3-orchestrator/provisioning/*.yml',
+    'modules/totem-v3-orchestrator/totem-*.yml',
+    'modules/totem-v3-config-service/totem-*.yml',
     'package.json',
     'README.md'
   ]
@@ -26,8 +27,9 @@ gulp.task('package', ['clean'], () => {
   return gulp.src(paths.artifacts, {base: '.'})
     .pipe(rename(path => {
       path.dirname = path.dirname
-        .replace('modules/totem-v3-orchestrator/provisioning', '/templates')
-        .replace('modules/totem-v3/provisioning', '/templates');
+        .replace('modules/totem-v3-orchestrator', '/templates/orchestrator')
+        .replace('modules/totem-v3/provisioning', '/templates/cluster')
+        .replace('modules/totem-v3-config-service', '/templates/config-service');
     }))
     .pipe(gulp.dest(paths.dist));
 });
